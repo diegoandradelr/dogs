d06ce8f8207888fbc62c471400795e35d63c07e2
Diego Andrade <diegoalr@hotmail.com>
2026-07-08 15:33:07 -0300

    Validate user authentication token

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,15 @@
-// import React from 'react'
-
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
 import LoginForm from "./LoginForm.jsx";
 import LoginCreate from "./LoginCreate.jsx";
 import LoginPasswordLost from "./LoginPasswordLost.jsx";
 import LoginPasswordReset from "./LoginPasswordReset.jsx";
+import { UserContext } from "../../UserContext";
 
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
   return (
     <div>
       <Routes>
