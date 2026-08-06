77e938b47fb5cb99a37e7f84cd17b8fcfcdc8ad5
Diego Andrade <diegoalr@hotmail.com>
2026-08-06 11:46:29 -0300

    Implement photo feed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return <div className={styles.footer}></div>;
 };
 
 export default Footer;
